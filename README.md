b0af69ae6e201136ade910e3a98a0087e331b403
aisuko <urakiny@gmail.com>
2021-10-31 10:49:02 +0800

    Add rocker
    Signed-off-by: aisuko <urakiny@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # package
+
+
+## launcher
+
+* Rust
+
+
+## command
+
+* Bash
+* Shell
+
+
+## Setup docker on Ubuntu20.04
+
+Thanks to Digital Ocean
+
+https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04
---
+++ bocker/bocker.sh
@@ -0,0 +1,62 @@
+#! /usr/bin/env bash
+#------------------------------------------
+# copyright(c) Aisuko. All rights reverved.
+# Licensed under the GNU GENERAL PUBLIC LICENSE.
+#------------------------------------------
+#
+# Maintainer: Aisuko
+#
+#
+
+
+set -e
+
+USERNAME=${1:-"azureuser"}
+
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e "Please run as root that would be good."
+    exit 1
+fi
+
+apt_get_update_if_needed()
+{
+    if  [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        sudo apt-get update
+    else
+        echo "Skipping the update."
+    fi
+}
+
+# Update
+apt_get_update_if_needed
+
+
+package_list="apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common"
+
+
+echo "Install packages: ${package_list}"
+sudo apt install -y ${package_list}
+
+
+UBUNTU_GPG="https://download.docker.com/linux/ubuntu/gpg"
+
+curl -fsSL ${UBUNTU_GPG} | sudo apt-key add -
+
+echo "ADD docker stable repo"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+
+echo "Install the latest version docker-ce"
+sudo apt install -y docker-ce
+
+# Add current user to the docker group
+sudo usermod -aG docker ${USERNAME}
+
+echo "Installed docker, show the status with the command `sudo systemctl status docker`, 
+    please log out under current user manually in order to use docker command without root."
+
